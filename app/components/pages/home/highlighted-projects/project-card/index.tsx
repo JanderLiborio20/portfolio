a88bc7ca9a7422631e0b4104d7133cb45bfc890c
jander.liborio <jandernunes14@gmail.com>
2024-08-23 00:44:07 -0400

    create projetc card

---
+++ app/components/pages/home/highlighted-projects/project-card/index.tsx
@@ -0,0 +1,43 @@
+import { TechBadge } from '@/app/components/tech-badge';
+import Image from 'next/image';
+
+export const ProjectCard = () => {
+  return (
+    <div className="flex gap-6 lg:gap-12 flex-col lg:flex-row">
+      <div className="w-full h-full">
+        <Image
+          width={420}
+          height={304}
+          src="https://media.graphassets.com/FRhUdgUQTHmLmwf9u0BA"
+          alt="Thumbnail do projeto BookWise"
+          className="w-full h-[200px] sm:h-[300px] lg:w-[420px] lg:min-h-full object-cover rounded-lg"
+        />
+      </div>
+
+      <div>
+        <h3 className="flex items-center gap-3 font-medium text-lg text-gray-50">
+          <Image
+            width={20}
+            height={20}
+            alt=""
+            src="/images/icons/project-title-icon.svg"
+          />
+          BookWise
+        </h3>
+
+        <p className="text-gray-400 my-6">
+          BookWise é uma plataforma de avaliação de livros que foi desenvolvida
+          durante o bootcamp Ignite da Rocketseat. Com apenas um Figma
+          precisávamos desenvolver essa aplicação completa Full Stack com
+          Next.js.
+        </p>
+
+        <div className="flex gap-x-2 gap-y-3 flex-wrap mb-8 lg:max-w-[350px]">
+          {Array.from({ length: 6 }).map((_, index) => (
+            <TechBadge key={index} name="Nest.js" />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
