f33858f86609185a3eaea4dffb3593001369f303
jander.liborio <jandernunes14@gmail.com>
2024-08-22 23:41:46 -0400

    hero section finalization

--- app/components/pages/hero-section/index.tsx
+++ app/components/pages/hero-section/index.tsx
@@ -1,9 +1,24 @@
 import Image from 'next/image';
 import { TechBadge } from '../../tech-badge';
+import { Button } from '../../button';
+import { HiArrowNarrowRight } from 'react-icons/hi';
+import {
+  TbBrandGithub,
+  TbBrandLinkedin,
+  TbBrandYoutube,
+  TbBrandWhatsapp,
+} from 'react-icons/tb';
+
+const MOCK_CONTACTS = [
+  { url: 'http://github.com.br', icon: <TbBrandGithub /> },
+  { url: 'http://github.com.br', icon: <TbBrandLinkedin /> },
+  { url: 'http://github.com.br', icon: <TbBrandYoutube /> },
+  { url: 'http://github.com.br', icon: <TbBrandWhatsapp /> },
+];
 
 export const HeroSection = () => {
   return (
-    <section className="w-full h-[755px] bg-hero-image bg-cover bg-center bg-no-repeat flex flex-col justify-end pb-10 sm:pb-32 py-32 lg:pb-[110px]">
+    <section className="w-full lg:h-[755px] bg-hero-image bg-cover bg-center bg-no-repeat flex flex-col justify-end pb-10 sm:pb-32 py-32 lg:pb-[110px]">
       <div className="container flex items-start justify-between flex-col-reverse lg:flex-row">
         <div className="w-full lg:max-w-[530px]">
           <p className="font-mono text-emerald-400 ">Olá, meu nome é</p>
@@ -22,13 +37,32 @@ export const HeroSection = () => {
             ))}
           </div>
 
-          <div>contato</div>
+          <div className="mt-6 lg:mt-10 flex sm:items-center sm:gap-5 flex-col sm:flex-row">
+            <Button className="w-max shadow-button">
+              Entre em contato <HiArrowNarrowRight size={18} />
+            </Button>
+
+            <div className="text-2xl text-gray-600 flex items-center h-20 gap-3">
+              {MOCK_CONTACTS.map((contact, index) => (
+                <a
+                  href={contact.url}
+                  key={`contact-${index}`}
+                  target="_blank"
+                  className="hover:text-gray-100 transition-colors"
+                  rel="noreferrer"
+                >
+                  {contact.icon}
+                </a>
+              ))}
+            </div>
+          </div>
         </div>
         <Image
           width={420}
           height={404}
-          src="/images/profile-pic.png"
+          src="/images/fotojped.jpeg"
           alt="foto de perfil do jander liborio"
+          className="w-[300px] h-[300px] lg:w-[420px] lg:h-[404px] mb-6 lg:mb-0 shadow-2xl rounded-lg object-cover"
         />
       </div>
     </section>
