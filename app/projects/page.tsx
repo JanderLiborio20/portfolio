b5455b3128dca766a070df86cca4d7c9176c90f6
jander.liborio <jandernunes14@gmail.com>
2024-08-24 14:51:06 -0400

    project list

---
+++ app/projects/page.tsx
@@ -0,0 +1,11 @@
+import { PageIntroduction } from '../components/pages/projects/page-introcduction';
+import { ProjectsList } from '../components/pages/projects/projects-list';
+
+export default function Projects() {
+  return (
+    <>
+      <PageIntroduction />
+      <ProjectsList />
+    </>
+  );
+}
