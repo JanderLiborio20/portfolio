d3711337cc66ad29b76d872aaf60a97a00ab68a6
jander.liborio <jandernunes14@gmail.com>
2024-08-25 23:42:08 -0400

    create api hygraph

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,40 @@ import { HeroSection } from './components/pages/home/hero-section';
 import { HighlightedProject } from './components/pages/home/highlighted-projects';
 import { KnownTechs } from './components/pages/home/known-techs';
 import { WorkExperience } from './components/pages/home/work-experience';
+import { fetchHygraphQuery } from './utils/fetch-hygraph-query';
+
+const getPageData = async () => {
+  const query = `
+    query PageInfoQuery {
+      page(where: {slug: "home"}) {
+        introduction {
+          raw
+        }
+        technologies {
+          name
+        }
+        profilePicture {
+          url
+        }
+        socials {
+          url
+          iconSvg
+        }
+        knownTechs {
+          iconSvg
+          name
+          startDate
+        }
+      }
+    }
+  `;
+
+  return fetchHygraphQuery(query);
+};
 
 export default async function Home() {
+  const response = await getPageData();
+  console.log('response: ', response);
   return (
     <>
       <HeroSection />
